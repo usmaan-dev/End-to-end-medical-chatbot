cf7542c326b27dabf1ee5628cc838e60310c2de4
Ahmad Raza <h.ahmadraz@gmail.com>
2024-12-24 18:37:23 +0500

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative ai Project',
+    version='0.0.0',
+    author='Usman Afzal',
+    author_email='usmaanafzal047@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
